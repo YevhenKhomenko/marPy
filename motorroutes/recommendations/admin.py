6a417315e7db33e1d0a223e44c9aa2d9b46740e1
xml12333 <xml12333@gmail.com>
2021-04-06 21:30:02 +0300

    init app recommendation and create model

---
+++ motorroutes/recommendations/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Similarity,Place,OnlineLink
+
+
+admin.site.register(Place)
+
+admin.site.register(Similarity)
+
+admin.site.register(OnlineLink)
