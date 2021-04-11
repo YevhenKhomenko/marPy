1509ec96ae1d66bbf61f836aca609f2b2229ec4c
xml12333 <xml12333@gmail.com>
2021-04-11 10:39:26 +0300

    chenge order in installed apps

--- motorroutes/motorroutes/settings.py
+++ motorroutes/motorroutes/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'recommendations.apps.RecommendationsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
