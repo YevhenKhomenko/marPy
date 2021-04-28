1a01e49ec4137f889653d3874e466cc1658789f7
IlliaHudilin <illiahudilin@gmail.com>
2021-04-28 09:30:31 +0300

    change DB to postgresql

--- motorroutes/motorroutes/settings.py
+++ motorroutes/motorroutes/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'motorroutes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'motorroutes',
+        'USER': 'motorroutes',
+        'PASSWORD': 'motorroutes',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
