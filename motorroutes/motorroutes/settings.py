c6326b0cde03847579cc1f093bc84a97c79cbcfb
DianaVish <diana33133@gmail.com>
2021-05-05 20:26:27 +0300

    migrations

--- motorroutes/motorroutes/settings.py
+++ motorroutes/motorroutes/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'motorroutes.wsgi.application'
 
 DATABASES = { 
     'default': { 
-        'ENGINE': 'django.db.backends.sqlite3', 
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'motorroutes', 
+        'USER': 'Diana',
+        'PASSWORD': 't347009003t',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
