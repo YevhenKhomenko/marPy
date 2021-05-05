9c09b6464912fec52794c3d609732ddb66cacef6
xml12333 <xml12333@gmail.com>
2021-05-05 18:42:49 +0300

    add skiped files in commit

--- motorroutes/accounts/serializers.py
+++ motorroutes/accounts/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import UserProfile
 
+class ValidationMixIn():
+    def validate_userprofile(self, data):
+        u_count = UserProfile.objects.filter(id=data.get('id')).count()
+        if u_count == 1:
+            return data
+        else:
+            raise serializers.ValidationError("wrong UserProfile id")
 
-class UserProfileNestedSerializer(serializers.ModelSerializer):
+class UserProfileNestedSerializer(serializers.ModelSerializer,ValidationMixIn):
     id = serializers.IntegerField()
     date_of_birth = serializers.DateField(read_only=True)
     phone_number = serializers.CharField(read_only=True)
--- motorroutes/recommendations/filters.py
+++ motorroutes/recommendations/filters.py
@@ -1,20 +1,19 @@
 import django_filters
 from django.db.models import Q
 from django_filters.rest_framework import FilterSet
-from .models import Place,Similarity,OnlineLink
+from .models import Place
 
 
 class PlaceFilter(FilterSet):
 
     def search_filter(self, qs, name, value):
         return qs.filter(
-            Q(title__icontains=value)|Q(description__icontains=value)
+            Q(title__icontains=value) | Q(description__icontains=value)
         )
 
-
     search = django_filters.filters.CharFilter(method='search_filter')
+
     #
     class Meta:
         model = Place
-        fields = ['search',]
-
+        fields = ['search', ]
